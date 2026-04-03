716b14c0d8965039e4a07c181c4a28c09d0b4c64
Ouanes._.med <soulgaboytellem0942i@gmail.com>
2026-04-03 19:49:19 +0100

    feat: initialize better-auth configuration with database adapter and custom user roles

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,7 +14,12 @@ export const auth = betterAuth({
   database: dbAdapter,
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-  trustedOrigins: ["https://dash.better-auth.com", "http://localhost:3000", "https://combined-judges-health-known.trycloudflare.com"],
+  trustedOrigins: [
+    "https://dash.better-auth.com", 
+    "http://localhost:3000", 
+    "https://combined-judges-health-known.trycloudflare.com",
+    "https://health-os-one.vercel.app"
+  ],
   emailAndPassword: {
     enabled: true,
   },
